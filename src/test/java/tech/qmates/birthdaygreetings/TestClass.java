8ed79ca1e9c6be4b51868a68c84d010c9b0b82bc
eparlato <edoardo.parlato@qmates.tech>
2026-01-14 16:47:15 +0100

    First commit with working junit5 test

---
+++ src/test/java/tech/qmates/birthdaygreetings/TestClass.java
@@ -0,0 +1,12 @@
+package tech.qmates.birthdaygreetings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestClass {
+    @Test
+    void itWorks() {
+        assertThat(true).isTrue();
+    }
+}
